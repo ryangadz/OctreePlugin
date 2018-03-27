9065761e5ce1d92b9bb059ea6e838ce48566fc01
ryangadz@msn.com <ryangadz@msn.com>
2018-03-27 14:07:12 -0700

    initial plugin setup

---
+++ Source/OctreePlugin/Private/OctreePluginBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "OctreePluginBPLibrary.h"
+#include "OctreePlugin.h"
+
+UOctreePluginBPLibrary::UOctreePluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UOctreePluginBPLibrary::OctreePluginSampleFunction(float Param)
+{
+	return -1;
+}
+
